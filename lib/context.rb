8608b9eb67c171db7be9acd7502658ea716e082f
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2026-01-06 11:17:05 +0100

    some tweaks to the homepage

--- lib/context.rb
+++ lib/context.rb
@@ -7,8 +7,9 @@ module Edward
 
     def include file, locals = {}
       include_path = "_include/" + file
+      yaml, content = Page.extract_front_matter(File.read(include_path))
       if File.exist? include_path
-        Tilt.new(include_path).render(self, { local: locals }) { yield }
+        Tilt[include_path].new(nil, nil, yaml&.dig(:options)){ content }.render(self, { local: locals }) { yield }
       else
         raise "#{file} not found in _include"
       end
